7b3edcb4795741a401aa78d4453f19a5bfde9859
SlimyZockt <slimyzockt77@gmail.com>
2025-08-10 12:24:21 +0200

    mooncake

---
+++ README.md
@@ -0,0 +1,4 @@
+# MoonCakeNvim 🥮
+
+
+This is my own Config. Inspired by [this video](https://www.youtube.com/watch?v=xGkL2N8w0H4).
--- init.lua
+++ init.lua
@@ -96,6 +96,7 @@ vim.pack.add({
     { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = "main" },
     { src = "https://github.com/chomosuke/typst-preview.nvim" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/L3MON4D3/LuaSnip" },
     { src = "https://github.com/echasnovski/mini.ai" },
