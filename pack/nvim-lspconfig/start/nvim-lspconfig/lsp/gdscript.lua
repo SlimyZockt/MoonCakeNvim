343ed85d1d792a8d5343c869a0f20b473241319f
SlimyZockt <slimyzockt77@gmail.com>
2025-08-11 23:24:12 +0200

    fix godot

--- pack/nvim-lspconfig/start/nvim-lspconfig/lsp/gdscript.lua
+++ pack/nvim-lspconfig/start/nvim-lspconfig/lsp/gdscript.lua
@@ -4,12 +4,6 @@
 ---
 --- Language server for GDScript, used by Godot Engine.
 
-local gdproject = io.open(vim.fn.getcwd() .. '/project.godot', 'r')
-if gdproject then
-  io.close(gdproject)
-  -- vim.fn.serverstart './godothost'
-  vim.lsp.enable("gdscript")
-end
 
 local port = os.getenv 'GDScript_Port' or '6005'
 local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
